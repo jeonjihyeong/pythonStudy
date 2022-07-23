749628e9ef49986fd8e8d06771f3402e7a47dea0
jeongminju0815 <jmju0815@gmail.com>
2022-07-24 00:00:48 +0900

    7.23 조합

--- 민주/B15649.py
+++
@@ -1,9 +0,0 @@
-from itertools import permutations
-n,m = map(int,input().split())
-
-temp = []
-for i in permutations(range(1,n+1),m):
-    temp = list(i)
-    print(' '.join(str(s) for s in temp))
-
-
--- 민주/수학/B6603.py
+++ 민주/수학/B6603.py
@@ -1,30 +1,8 @@
-# from itertools import combinations
-# while True:
-#     arr = list(map(int,input().split()))
-#     if arr[0] == 0:
-#         break
-#     for i in combinations(arr[1:],6):
-#         print(' '.join(map(str,list(i))))
-#     print()
-
-def combination(idx, temp):
-    if len(temp) == 6:
-        answer.append(temp[:])
-        return
-    for i in range(idx, arr[0]):
-        combination(i+1,temp+[arr[i+1]])
-    
-
-
+from itertools import combinations
 while True:
-    answer = []
     arr = list(map(int,input().split()))
     if arr[0] == 0:
         break
-    combination(0,[])
-    print(answer)
-    for i in answer:
-        print(" ".join(map(str,i)))
+    for i in combinations(arr[1:],6):
+        print(' '.join(map(str,list(i))))
     print()
-    
-    
